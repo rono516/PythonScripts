72444a78fb99460f5549882077309e2cb9b19c46
rono516 <ronocollins2000@gmail.com>
2021-11-29 05:55:29 +0300

    even odd game

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+2547", "Like our last day", 22, 19)
+pywhatkit.sendwhatmsg("+254792691567", "I love you bae boo", 22, 38)
+'''message and email alerts https://www.youtube.com/watch?v=B1IsCbXp0uE'''
